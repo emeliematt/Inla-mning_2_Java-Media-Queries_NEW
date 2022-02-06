c306d9e4009c37d4945245b0e3f9ffa2f6bdc650
First Last <yourname@domain.com>
2022-02-06 11:21:31 +0100

    new_2

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+let btnClick_here_for_project = document.querySelector('#click_here');
 
+
+btnClick_here_for_project.addEventListener('click', () => btnClick_here_for_project.style.backgroundColor = '#1a26d4')
+
+
+/* en 'click' funktion som är kopplad till min button i html - den ändrar färg till blå när en klickar på button*/
